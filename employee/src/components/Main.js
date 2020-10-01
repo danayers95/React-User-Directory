6c07e51753c32e626ceba897861d72c0a9c6999b
Donkey <danglayers@gmail.com>
2020-10-01 15:24:03 -0400

    added files for the wrapper, navbar, and main

---
+++ employee/src/components/Main.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Area from "./Area.js";
+import "../styles/Main.css";
+
+export default class Main extends Component {
+    render() {
+      return (
+        <>
+          <Area />
+        </>
+      );
+    }
+  }
