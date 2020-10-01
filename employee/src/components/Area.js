6e435dbc0d4330ee1b48f347a715c7c7acde6d0c
Donkey <danglayers@gmail.com>
2020-10-01 17:56:29 -0400

    fixed bug in Area.js

--- employee/src/components/Area.js
+++ employee/src/components/Area.js
@@ -13,5 +13,88 @@ export default class Area extends Component {
         { name: "DOB", width: "10%" },
     ];
 
+    state = {
+        users: [{}],
+        order: "descend",
+        filteredUsers: [{}],
+      };
     
+      
+    
+      handleSort = (heading) => {
+        if (this.state.order === "descend") {
+          this.setState({
+            order: "ascend",
+          });
+        } else {
+          this.setState({
+            order: "descend",
+          });
+        }
+    
+        const compareFnc = (a, b) => {
+          if (this.state.order === "ascend") {
+            if (a[heading] === undefined) {
+              return 1;
+            } else if (b[heading] === undefined) {
+              return -1;
+            }
+           
+            else if (heading === "name") {
+              return a[heading].first.localeCompare(b[heading].first);
+            } else {
+              return a[heading] - b[heading];
+            }
+          } else {
+            
+            if (a[heading] === undefined) {
+              return 1;
+            } else if (b[heading] === undefined) {
+              return -1;
+            }
+           
+            else if (heading === "name") {
+              return b[heading].first.localeCompare(a[heading].first);
+            } else {
+              return b[heading] - a[heading];
+            }
+          }
+        };
+        const sortedUsers = this.state.filteredUsers.sort(compareFnc);
+        this.setState({ filteredUsers: sortedUsers });
+      };
+    
+      handleSearchChange = (event) => {
+        console.log(event.target.value);
+        const filter = event.target.value;
+        const filteredList = this.state.users.filter((item) => {
+          let values = Object.values(item).join("").toLowerCase();
+          return values.indexOf(filter.toLowerCase()) !== -1;
+        });
+        this.setState({ filteredUsers: filteredList });
+      };
+    
+      componentDidMount() {
+        API.getUsers().then((results) => {
+          this.setState({
+            users: results.data.results,
+            filteredUsers: results.data.results,
+          });
+        });
+      }
+    
+      render() {
+        return (
+          <>
+            <Nav handleSearchChange={this.handleSearchChange} />
+            <div className="data-area">
+              <Table
+                headings={this.headings}
+                users={this.state.filteredUsers}
+                handleSort={this.handleSort}
+              />
+            </div>
+          </>
+        );
+      }
 }
